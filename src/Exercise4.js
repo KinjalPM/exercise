a816754b240d87f3eaf94530ee58d0bd7032c62a
Kinjal <kinjalpm96@gmail.com>
2021-02-23 18:05:13 -0500

    ex-4 changes

--- src/Exercise4.js
+++ src/Exercise4.js
@@ -1,6 +1,6 @@
 import './bestStyle.scss'
 import React from "react";
-
+import $ from 'jquery';
 
 const black = 'black';
 const  yellow= 'yellow';
@@ -9,20 +9,31 @@ class Exercise4 extends React.Component{
 
     constructor(props){
         super(props);
-        this.state = { color: black };
+        this.state = { 
+          color: black,
+          isToggleOn: "Click button to change it light theme"
+        };
         this.changeColor = this.changeColor.bind(this);
       }
-      changeColor() {
+  
+      changeColor(text) {
         const newColor = this.state.color == black ? yellow : black;
-        this.setState({ color: newColor });
+        const newisToggleOn = this.state.isToggleOn == "Click button to change it light theme" ? ' Click button to change it dark theme' : 'Click button to change it light theme'
+        this.setState({ 
+        color: newColor,
+        isToggleOn:newisToggleOn});
       }
+
+
       render(){
         return(
     <div style={{backgroundColor: 'white' , margin:'20px', padding:'20px'}}>
         <h1><em><u>Exercise 4:</u></em></h1>
           <div style={{background: this.state.color, height:'500px',boxSizing:'border-box'}}>
-          <h1 style={{color:'grey', margin:'10px'}}>theme</h1>
-          <button onClick={this.changeColor} style={{justifyItems:'baseline',marginTop:'432px'}}>Click to change theme</button>
+          <h1 style={{color:'grey', textAlign:'center',position:'relative',top:'49%'}}>{this.state.isToggleOn}</h1>
+          <button class= "btn " value="Light Theme" onClick={this.changeColor} style={{bottom:'0', right:'0',position:'absolute', marginRight: '40px', marginBottom:'25px', fontSize:'20px'}}>
+            "Click button"
+          </button>
           </div>
     </div>
         )
